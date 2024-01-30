3cf1c5e8ea64c091a3c7e1cec2578c5d02f73e45
aniket2727 <aniketkadam5950@gmail.com>
2024-01-30 16:56:01 +0530

    added api connection to home ui

--- client/src/componets/Home/FirstPage.js
+++ client/src/componets/Home/FirstPage.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaUser, FaPlus, FaThumbsUp } from 'react-icons/fa';
 import './firstpage.css';
 import image1 from './image1.jpeg';
+import { getposts } from '../Api/getpostsapi';
 
 const FirstPage = () => {
     const timesToRender = 5;
     const contentArray = Array.from({ length: timesToRender });
+
+    const [postCollections, setPostCollection] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const dataFromApi = await getposts();
+                setPostCollection(dataFromApi);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []); // The empty dependency array ensures that useEffect runs once when the component mounts
 
     return (
         <div className='main-postx'>
@@ -19,10 +35,9 @@ const FirstPage = () => {
                     </div>
                     <div className='main-post'>
                         <div className='main-post-image'>
-                            <img src={image1} alt="Description" /> {/* Added alt attribute for accessibility */}
-                        </div>   
+                            <img src={image1} alt="Description" /> {/* Replace with actual image data */}
+                        </div>
                     </div>
-
                     <div className='main-likes'>
                         <button><FaThumbsUp /> Like</button>
                     </div>    
