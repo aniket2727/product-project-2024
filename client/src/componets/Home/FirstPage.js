bb47dcba0e22ff7c791a315c423cdc745dc9c583
aniket2727 <aniketkadam5950@gmail.com>
2024-01-30 16:39:54 +0530

    added home  page ui and css

--- client/src/componets/Home/FirstPage.js
+++ client/src/componets/Home/FirstPage.js
@@ -1,15 +1,34 @@
+import React from 'react';
 
 
 
-import React from 'react'
-import DisplayUserProfile from '../aboutinfo/DisplayUserProfile'
+import { FaUser, FaPlus } from 'react-icons/fa';
+import './firstpage.css';
+import image1 from './image1.jpeg';
 
 const FirstPage = () => {
-  return (
-    <div>
-          
-    </div>
-  )
+    const timesToRender = 5;
+    const contentArray = Array.from({ length: timesToRender });
+
+    return (
+        <div className='main-postx'>
+            {contentArray.map((_, index) => (
+                <div key={index} className='main-post-datax'>
+                    <div className='main-namex'>
+                        <div className='profile-info'>
+                            <h3><FaUser/> Aniket Kadam</h3>
+                        </div>
+                        <button><FaPlus /> Connect</button>
+                    </div>
+                    <div className='main-post'>
+                        <div className='main-post-image'>
+                        <img src={image1} alt="Description" /> {/* Added alt attribute for accessibility */}
+                        </div>   
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default FirstPage
+export default FirstPage;
