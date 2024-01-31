a8b86ea3695750008c35e368ff1db8e579be8460
aniket2727 <aniketkadam5950@gmail.com>
2024-01-31 08:12:01 +0530

    make connections for data in ui componets

--- client/src/componets/connectionUser/Suggestion.js
+++ client/src/componets/connectionUser/Suggestion.js
@@ -1,15 +1,31 @@
-
-
-
-
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { getalluseraccounts } from '../Api/Connectionsuggestion';
 
 const Suggestion = () => {
+  const [alluserprofiledata, setalluserprofiledata] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Assuming getalluseraccounts() returns a Promise
+        const result = await getalluseraccounts();
+        setalluserprofiledata(result); // Assuming the data structure returned has a "data" property
+      } catch (error) {
+        console.error('Error fetching user accounts:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      
+      {/* Render your UI with the fetched data */}
+      {alluserprofiledata.map((user) => (
+        <div key={user.id}>{/* Render user data here */}</div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Suggestion
+export default Suggestion;
